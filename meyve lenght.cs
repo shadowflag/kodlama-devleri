db3251354a76e3c58cccbe8bbd20c1af2f5c067a
muhammed madenci <muhammedbilal5453@gmail.com>
2025-12-05 18:51:31 +0300

    Add files via upload

---
+++ meyve lenght.cs
@@ -0,0 +1,9 @@
+string[] meyve = { "domates","biber","patlican"};
+        
+        int i = 0;
+        while(i < meyve.Length);
+        {
+            Console.WriteLine(meyve[i]);
+            i++;
+        }
+    
